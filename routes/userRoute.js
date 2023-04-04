350e29b4f2b5534ae092a4d8a59d3fbfe105af03
ahmetoztrk <ahmetoztrk1709@gmail.com>
2023-04-04 22:33:06 +0300

    added custom middlewares

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const authController = require('../controllers/authController');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
 router.route('/signup').post(authController.createUser); //localhost:3000/users/signup
-router.route('/login').post(authController.loginUser);  //localhost:3000/users/login
+router.route('/login').post(authController.loginUser); //localhost:3000/users/login
 router.route('/logout').get(authController.logoutUser); //localhost:3000/users/logout
-router.route('/dashboard').get(authController.getDashboardPage); //localhost:3000/users/dashboard
+router.route('/dashboard').get(authMiddleware, authController.getDashboardPage); //localhost:3000/users/dashboard
 
 module.exports = router;
