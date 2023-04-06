87afb0fe59214964e21c6cef2fa441fdca76b088
ahmetoztrk <ahmetoztrk1709@gmail.com>
2023-04-06 03:06:42 +0300

    added node-mailer

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,24 +1,72 @@
-exports.getIndexPage = (req,res) => {
-    console.log(req.session.userID);
-    res.status(200).render('index' , {
-        page_name : 'index'
-    })
-}
+const nodemailer = require('nodemailer');
 
-exports.getAboutPage = (req,res) => {
-    res.status(200).render('about',{
-        page_name : 'about'
-    })
-}
+exports.getIndexPage = (req, res) => {
+  console.log(req.session.userID);
+  res.status(200).render('index', {
+    page_name: 'index',
+  });
+};
 
-exports.getRegisterPage = (req,res) => {
-    res.status(200).render('register',{
-        page_name : 'register'
-    })
-}
+exports.getAboutPage = (req, res) => {
+  res.status(200).render('about', {
+    page_name: 'about',
+  });
+};
 
-exports.getLoginPage = (req,res) => {
-    res.status(200).render('login',{
-        page_name : 'login'
-    })
-}
+exports.getRegisterPage = (req, res) => {
+  res.status(200).render('register', {
+    page_name: 'register',
+  });
+};
+
+exports.getLoginPage = (req, res) => {
+  res.status(200).render('login', {
+    page_name: 'login',
+  });
+};
+
+exports.getContactPage = (req, res) => {
+  res.status(200).render('contact', {
+    page_name: 'contact',
+  });
+};
+
+exports.sendMail = async (req, res) => {
+  const outputMessage = `
+    <h1>Mail Details</h1>
+    <ul>
+        <li>Name: ${req.body.name}</li>
+        <li>Email: ${req.body.email}</li>
+    </ul>
+    <h1>Message</h1>
+    <p>${req.body.message}</p>
+    `;
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: "ahmetoztrk1709@gmail.com", // generated ethereal user
+      pass: "***", // generated ethereal password
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"SmartEdu Contact Form" <ahmetoztrk1709@gmail.com>', // sender address
+    to: '***@gmail.com', // list of receivers
+    subject: 'SmartEdu Contact New Message ✔', // Subject line
+    html: outputMessage, // html body
+  });
+
+  console.log('Message sent: %s', info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+  res.status(200).redirect("/contact");
+};
