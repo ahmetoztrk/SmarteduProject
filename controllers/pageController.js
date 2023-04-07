a72870d255972cfaf25541324adaee0fcf3c3dd0
ahmetoztrk <ahmetoztrk1709@gmail.com>
2023-04-07 17:44:15 +0300

    configured flash messages

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -32,7 +32,8 @@ exports.getContactPage = (req, res) => {
 };
 
 exports.sendMail = async (req, res) => {
-  const outputMessage = `
+  try {
+    const outputMessage = `
     <h1>Mail Details</h1>
     <ul>
         <li>Name: ${req.body.name}</li>
@@ -42,31 +43,36 @@ exports.sendMail = async (req, res) => {
     <p>${req.body.message}</p>
     `;
 
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, // true for 465, false for other ports
-    auth: {
-      user: "ahmetoztrk1709@gmail.com", // generated ethereal user
-      pass: "***", // generated ethereal password
-    },
-  });
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true, // true for 465, false for other ports
+      auth: {
+        user: 'ahmetoztrk1709@gmail.com', // generated ethereal user
+        pass: '<password>', // generated ethereal password
+      },
+    });
 
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: '"SmartEdu Contact Form" <ahmetoztrk1709@gmail.com>', // sender address
-    to: '***@gmail.com', // list of receivers
-    subject: 'SmartEdu Contact New Message ✔', // Subject line
-    html: outputMessage, // html body
-  });
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: '"SmartEdu Contact Form" <ahmetoztrk1709@gmail.com>', // sender address
+      to: '<email>@gmail.com', // list of receivers
+      subject: 'SmartEdu Contact New Message ✔', // Subject line
+      html: outputMessage, // html body
+    });
 
-  console.log('Message sent: %s', info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    console.log('Message sent: %s', info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
-  // Preview only available when sending through an Ethereal account
-  console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    // Preview only available when sending through an Ethereal account
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 
-  res.status(200).redirect("/contact");
+    req.flash('success', 'We received your message successfully');
+    res.status(200).redirect('/contact');
+  } catch (err) {
+    req.flash('error', 'Something happened!');
+    res.status(400).redirect('/contact');
+  }
 };
